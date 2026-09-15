3f2584e239ac7c358a8e529389652ac5cc03d8b8
turtletask-release <release@turtletask.app>
2026-09-15 16:44:29 +0000

    turtletask 0.1.0

---
+++ Casks/turtletask.rb
@@ -0,0 +1,20 @@
+cask "turtletask" do
+  version "0.1.0"
+  sha256 "9f030a744e5bc49c2f673c42506e96dcfb5bdc0ff1c85de73880ad024c94056b"
+
+  url "https://github.com/raindanc-ing/homebrew-turtletask/releases/download/desktop-v#{version}/TurtleTask_#{version}_universal.dmg"
+  name "TurtleTask"
+  desc "Spotlight-style palette for the TurtleTask task board"
+  homepage "https://turtletask.app/"
+
+  depends_on macos: ">= :catalina"
+
+  app "TurtleTask.app"
+
+  zap trash: [
+    "~/Library/Application Support/app.turtletask.desktop",
+    "~/Library/Caches/app.turtletask.desktop",
+    "~/Library/WebKit/app.turtletask.desktop",
+    "~/Library/Saved Application State/app.turtletask.desktop.savedState",
+  ]
+end
